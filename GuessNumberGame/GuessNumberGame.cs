d0da9c38f81520f1c6181a1b740705b49732c5d7
Ziyaddin <ziya.huseynov2001@mail.ru>
2024-02-15 20:36:15 +0400

    Обьявил неизменяемые поля и создал метод старт

--- GuessNumberGame/GuessNumberGame.cs
+++ GuessNumberGame/GuessNumberGame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +14,37 @@ namespace GuessNumberGame
     }
     public class GuessNumberGame
     {
+        private readonly int max;
+        private readonly int maxTries;
+        private readonly GuessingPlayer guessingPlayer;
+
         public GuessNumberGame(int max = 100, int maxTries = 5, GuessingPlayer guessingPlayer = GuessingPlayer.Human)
         {
-            
+            this.max = max;
+            this.maxTries = maxTries;
+            this.guessingPlayer = guessingPlayer;
+        }
+
+        public void Start()
+        {
+            if(guessingPlayer == GuessingPlayer.Human)
+            {
+                HumanGuesses();
+            }
+            else
+            {
+                MachineGuesses();
+            }
+        }
+
+        private void HumanGuesses()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MachineGuesses()
+        {
+            throw new NotImplementedException();
         }
     }
 }
