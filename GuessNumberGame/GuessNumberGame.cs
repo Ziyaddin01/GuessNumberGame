709807c16afb5a7f194dc909950508a63a0143e1
Ziyaddin <ziya.huseynov2001@mail.ru>
2024-02-15 19:32:35 +0400

    Добавьте файлы проекта.

---
+++ GuessNumberGame/GuessNumberGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumberGame
+{
+    public enum GuessingPlayer
+    {
+        Human,
+        Machine
+    }
+    public class GuessNumberGame
+    {
+        public GuessNumberGame(int max = 100, int maxTries = 5, GuessingPlayer guessingPlayer = GuessingPlayer.Human)
+        {
+            
+        }
+    }
+}
---
+++ GuessNumberGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
